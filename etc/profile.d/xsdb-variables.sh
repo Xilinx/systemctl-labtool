915afd944756b48747dae6319ef97f258afbc950
vikhyatg <vikhyat.goyal@amd.com>
2025-01-28 18:41:54 -0700

    Add tcl lib to /opt path

--- etc/profile.d/xsdb-variables.sh
+++ etc/profile.d/xsdb-variables.sh
@@ -1,5 +1,6 @@
 export XILINX_VITIS=/usr/local/xilinx_vitis
 export PATH=${PATH}:${XILINX_VITIS}
 export TCLLIBPATH=/usr/local/xilinx_vitis
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export TCL_LIBRARY=/opt/labtools/lib/tcl8.6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/labtools/lib/
 export LC_ALL='en_US.UTF-8'
