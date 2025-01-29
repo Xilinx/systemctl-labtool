1b610b3d9fa113dc71189bcf901158a4e1e5a302
vikhyatg <vikhyat.goyal@amd.com>
2025-01-28 20:33:46 -0700

    Move all labtools related files to /opt

--- etc/profile.d/xsdb-variables.sh
+++ etc/profile.d/xsdb-variables.sh
@@ -1,6 +1,6 @@
-export XILINX_VITIS=/usr/local/xilinx_vitis
+export XILINX_VITIS=/opt/labtools/xilinx_vitis
 export PATH=${PATH}:${XILINX_VITIS}
-export TCLLIBPATH=/usr/local/xilinx_vitis
+export TCLLIBPATH=/opt/labtools/xilinx_vitis
 export TCL_LIBRARY=/opt/labtools/lib/tcl8.6
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/labtools/lib/
 export LC_ALL='en_US.UTF-8'
